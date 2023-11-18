4194906ada8e2bc7471dda7a5bd646ce67d84f40
Roc Tanweer <mohammadtanweer769@gmail.com>
2023-11-18 12:59:23 +0530

    operations on SLL

--- linkedList/operations.c
+++ linkedList/operations.c
@@ -29,6 +29,23 @@ struct Node *insertAtBeginning(struct Node *head, int element)
     return head;
 }
 
+// function to delete at the beginning
+struct Node *deleteAtBeginning(struct Node *head)
+{
+    if (head == NULL)
+    {
+        printf("List is empty. Cannot delete.\n");
+        return head;
+    }
+
+    struct Node *temp = head;
+
+    head = temp->next;
+
+    free(temp);
+    return head;
+}
+
 struct Node *insertAtEnd(struct Node *head, int element)
 {
     struct Node *newNode = createNode(element);
@@ -45,6 +62,31 @@ struct Node *insertAtEnd(struct Node *head, int element)
         temp = temp->next;
     }
     temp->next = newNode;
+
+    return head;
+}
+
+// function to delete at the end
+struct Node *deleteAtEnd(struct Node *head)
+{
+    if (head == NULL)
+    {
+        printf("List is empty. Cannot delete.\n");
+        return head;
+    }
+
+    struct Node *temp = head;
+
+    while (temp->next->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    struct Node *lastNode = temp->next;
+
+    temp->next = NULL;
+
+    free(lastNode);
 
     return head;
 }
@@ -78,6 +120,41 @@ struct Node *insertAtNthPos(struct Node *head, int element, int pos)
     return head;
 }
 
+// function to delete at nth position
+struct Node *deleteAtNthPos(struct Node *head, int pos)
+{
+    if (head == NULL)
+    {
+        printf("List is empty. Cannot delete.\n");
+        return head;
+    }
+
+    struct Node *temp = head;
+
+    if (pos == 1)
+    {
+        head = head->next;
+        free(temp);
+        return head;
+    }
+
+    for (int i = 1; i < pos - 1 && temp != NULL; i++)
+    {
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+    {
+        printf("Position out of rangen\n");
+        return head;
+    }
+
+    struct Node *deletedNode = temp->next;
+    temp->next = deletedNode->next;
+    free(deletedNode);
+    return head;
+}
+
 void printLinkedList(struct Node *head)
 {
     struct Node *temp = head;
@@ -104,6 +181,18 @@ int main()
     head = insertAtNthPos(head, 5, 1);
     head = insertAtNthPos(head, 4, 1);
     head = insertAtNthPos(head, 10, 6);
+
+    printLinkedList(head);
+
+    head = deleteAtBeginning(head);
+
+    printLinkedList(head);
+
+    head = deleteAtEnd(head);
+
+    printLinkedList(head);
+
+    head = deleteAtNthPos(head, 7);
 
     printLinkedList(head);
 
